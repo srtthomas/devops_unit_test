48146f95fb356a53405f690a0e8e8a3002f48d48
srtthomas <thomas.srt@gmail.com>
2024-04-02 21:58:17 +0200

    10.Unittest

--- unittests/src/main/java/com/example/unittests/RomanNumeralsKata.java
+++ unittests/src/main/java/com/example/unittests/RomanNumeralsKata.java
@@ -3,19 +3,24 @@ package com.example.unittests;
 public class RomanNumeralsKata {
 
     public static String convert(int number) {
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        if (number == 0) {
+            return "0 is not a roman number";
+        } else {
 
-        StringBuilder roman = new StringBuilder();
+            int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+            String[] symbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
 
-        for (int i = 0; i < values.length; i++) {
-            while (number >= values[i]) {
-                number -= values[i];
-                roman.append(symbols[i]);
+            StringBuilder roman = new StringBuilder();
+
+            for (int i = 0; i < values.length; i++) {
+                while (number >= values[i]) {
+                    number -= values[i];
+                    roman.append(symbols[i]);
+                }
             }
-        }
 
-        return roman.toString();
+            return roman.toString();
+        }
     }
 
 }
