514415135a006c2f36aeb78d9e8a4bee4fc2585d
srtthomas <thomas.srt@gmail.com>
2024-04-02 20:14:58 +0200

    inital project setup

---
+++ unittests/src/main/java/com/example/unittests/UnittestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestsApplication.class, args);
+	}
+
+}
---
+++ unittests/src/test/java/com/example/unittests/UnittestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnittestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
