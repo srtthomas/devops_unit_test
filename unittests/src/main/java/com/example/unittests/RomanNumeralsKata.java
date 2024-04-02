994a25db3b6adacc4860c9fce99723b240cb2638
srtthomas <thomas.srt@gmail.com>
2024-04-02 20:52:19 +0200

    1.Unittest

---
+++ unittests/src/main/java/com/example/unittests/RomanNumeralsKata.java
@@ -0,0 +1,10 @@
+package com.example.unittests;
+
+public class RomanNumeralsKata {
+    
+
+    public static String convert(int number) {
+        return "";
+}
+
+}
---
+++ unittests/src/test/java/com/example/unittests/RomanNumeralsKataTests.java
@@ -0,0 +1,15 @@
+package com.example.unittests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+
+
+public class RomanNumeralsKataTests {
+
+    @Test
+    void RomanNumeralKata1(){
+    assertEquals("I", RomanNumeralsKata.convert(1));
+
+}
+}
